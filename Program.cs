8a5fecc1a6295e86160e85666b67f6aa422c1623
A205PC26 <namn54589@gmail.com>
2024-07-10 10:34:22 +0700

    Add Program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2001207167_NguyenHoangNam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int index = LinearSearch(numbers, 5);
+            Console.WriteLine("The number 5 is at index " + index);
+        }
+
+        static int LinearSearch(int[] numbers, int target)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
